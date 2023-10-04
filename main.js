bde668a5bf001bb4d8efad5606779d1d75faf001
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-10-04 15:45:28 +0100

    set up structure of send query and closing connection

---
+++ main.js
@@ -0,0 +1,18 @@
+//import pg package
+import pg from "pg";
+
+// Get the connection string
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+// Create a new pool instance
+const pool = new pg.Pool({
+  connectionString,
+});
+
+// Send Query
+const data = await pool.query("SELECT * FROM authors;");
+const rows = data.rows;
+console.log(rows);
+
+// Close the connection
+await pool.end();
